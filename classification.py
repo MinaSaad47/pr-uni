056e342963a8d3193c49bc0ce0e57974fe41e601
MinaSaad47 <MinaSaad-47@outlook.com>
2022-05-21 20:19:18 +0200

    rename main to task name

--- classification.py
+++ classification.py
@@ -22,7 +22,7 @@ TEST_MOVIE_ACTOR_FILE    = 'movie-voice-actors-test-samples.csv'
 CLS = {
     "svm": ("SVM Model", SVMModel),
     "dt":  ("Decission Tree Model", DecisionTreeModel),
-    "pr":  ("Polynominal Regression Model", PolyRegModel),
+    "pr":  ("Polynominal Regressition Model", PolyRegModel),
     "lr":  ("Logistic Regression Model", LogisticRegressionModel),
 }
 
@@ -42,6 +42,8 @@ if __name__ == "__main__":
     train_parser = sub_parser.add_parser('bench')
     train_parser.add_argument('train_directory', type=str, help='Directory containing train dataset')
     train_parser.add_argument('test_directory', type=str, help='Directory containing test dataset')
+    train_parser.add_argument('-p', '--png', help='Output PNG file', action='store_true', dest='png_flag')
+    train_parser.add_argument('-w', '--win', help='Output Window', action='store_true', dest='win_flag')
     parsed_args = arg_parser.parse_args()
 
     # Train Action
@@ -116,13 +118,14 @@ if __name__ == "__main__":
         accuracies = []
 
         for i, model in enumerate(CLS.keys()):
-            print(f"[{i + 1}] {CLS[model][0]}")
+            print(f"[{i + 1}] {CLS[model][0]}:")
             cls = CLS[model][1](train_data)
             # timing train
             stime = time.time()
             cls.train()
             etime = time.time()
             train_time = etime - stime
+            print(f'\tTrain Time: {train_time} Sec')
             train_times.append(train_time)
 
             # timing test
@@ -130,39 +133,48 @@ if __name__ == "__main__":
             cls.predict(test_data)
             etime = time.time()
             test_time = etime - stime
+            print(f'\tTest Time: {test_time} Sec')
             test_times.append(test_time)
 
             # calculating accuracy
             accuracy = cls.test_accuracy()
+            print(f'\tAccuracy Time: {test_time} %')
             accuracies.append(accuracy)
 
         pos = np.arange(len(CLS.keys()))
 
-        fig = plt.figure()
-        plt.title("Train Times")
-        plt.xlabel('Models')
-        plt.ylabel('Time in Sec')
-        plt.bar(pos, train_times)
-        plt.xticks(pos, CLS.keys())
-        plt.savefig('train_times.png')
-        plt.show()
+        if parsed_args.png_flag or parsed_args.win_flag:
+            fig = plt.figure()
+            plt.title("Train Times")
+            plt.xlabel('Models')
+            plt.ylabel('Time in Sec')
+            plt.bar(pos, train_times)
+            plt.xticks(pos, CLS.keys())
+            if parsed_args.png_flag:
+                plt.savefig('train_times.png')
+            if parsed_args.win_flag:
+                plt.show()
 
-        fig = plt.figure()
-        plt.title("Test Times")
-        plt.xlabel('Models')
-        plt.ylabel('Time in Sec')
-        plt.bar(pos, test_times)
-        plt.xticks(pos, CLS.keys())
-        plt.savefig('test_times.png')
-        plt.show()
+            fig = plt.figure()
+            plt.title("Test Times")
+            plt.xlabel('Models')
+            plt.ylabel('Time in Sec')
+            plt.bar(pos, test_times)
+            plt.xticks(pos, CLS.keys())
+            if parsed_args.png_flag:
+                plt.savefig('test_times.png')
+            if parsed_args.win_flag:
+                plt.show()
 
 
-        fig = plt.figure()
-        plt.title("Accuracies")
-        plt.xlabel('Models')
-        plt.ylabel('Accuracy')
-        plt.bar(pos, accuracies)
-        plt.xticks(pos, CLS.keys())
-        plt.savefig('accuracies.png')
-        plt.show()
+            fig = plt.figure()
+            plt.title("Accuracies")
+            plt.xlabel('Models')
+            plt.ylabel('Accuracy')
+            plt.bar(pos, accuracies)
+            plt.xticks(pos, CLS.keys())
+            if parsed_args.png_flag:
+                plt.savefig('accuracies.png')
+            if parsed_args.win_flag:
+                plt.show()
 
